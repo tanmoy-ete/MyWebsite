e0c226fe6b3109dd2b5ec678dfad37ab66ef9303
Tanmoy Debnath <tanmoydebnath2204045@gmail.com>
2025-04-18 22:47:28 +0600

    form2

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -24,6 +24,7 @@ export default function Hero() {
           target="_blank"
           rel="noopener noreferrer"
           className="learn-more-btn"
+          id='btn1'
         >
           Hire me
         </a>
